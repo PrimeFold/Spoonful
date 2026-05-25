5d4f85353608aeb7abcab4ff8b34aeda744b0ded
Aditya Raj <cyberadi336@gmail.com>
2026-05-25 16:51:49 +0530

    feat : Implement basic express server - Setup basic prisma user schema - Setup better auth

---
+++ backend/src/server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import helmet from 'helmet';
+import dotenv from 'dotenv'
+import cors from 'cors';
+import { toNodeHandler } from 'better-auth/node';
+import { auth } from './lib/auth';
+dotenv.config();
+
+const app = express();
+const baseUrl = process.env.BASE_URL
+const PORT = process.env.PORT || 'http://localhost:3000'
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+app.use(express.json());
+app.use(helmet());
+app.use(cors({
+    origin:baseUrl
+}));
+
+app.listen(PORT,()=>{
+    console.log(`Server started running on port : ${PORT}`)
+})
